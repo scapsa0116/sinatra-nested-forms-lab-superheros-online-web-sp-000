a52b3dfee49d74b6ddbe8230a2aa2392052c497a
Stela Capsa <stela.capsa@gmail.com>
2020-04-06 13:45:35 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ get '/' do
 end 
 
    post '/teams' do 
-     binding.pry
+    # binding.pry
  @teams = Teams.new(name: params[:team][:name], motto: params[:team][:motto])
   members = params[:team][:members]
   
